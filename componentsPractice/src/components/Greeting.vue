6387f165d66a785270906c82ffc1dcfc5657bc59
Nikolaos Kouroumpetsis <nikolaos.kouroumpetsis@gmail.com>
2022-11-07 11:53:13 +0200

    first component

---
+++ componentsPractice/src/components/Greeting.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <p class="primary">{{ primaryMsg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Greeting",
+  data() {
+    return {
+      msg: "Hello World",
+      primaryMsg: "Class color",
+    };
+  },
+};
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+
+.primary {
+  color: blue;
+}
+</style>
